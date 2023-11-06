445dea5978089f8a1c60f0c68c950a26f71e4dd6
kwadwo-agyeman <sam300kye@gmail.com>
2023-11-06 09:06:26 -0600

    delete dummy.jsx. was just a dummy file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,6 @@ export default function Navbar(props){
     console.log(props)
     return(
         <nav className={props.darkMode ? "dark" : ""} >
-            <a href="/Dummy">dummy </a>
             <div className="nav--container">
                 <h3>STORE</h3>
                 <div className="toggle--container">
